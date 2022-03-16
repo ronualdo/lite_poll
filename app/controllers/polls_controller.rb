56bd566dd1907d22cf644a1c9d1f8f8dbb917939
ronualdo maciel <raxmac@gmail.com>
2022-03-16 01:32:59 -0300

    Adds routes

---
+++ app/controllers/polls_controller.rb
@@ -0,0 +1,17 @@
+class PollsController < ApplicationController
+  def show
+    double_poll = {
+      id: 1,
+      question: 'what is the best console?',
+      options: [
+        { id: 1, label: 'playstation', result: '0.95' },
+        { id: 2, label: 'xbox', result: '0.05' }
+      ]
+    }
+    render json: double_poll, status: 200
+  end
+
+  def update
+    render json: { id: 1, label: 'playstation' }, status: 200
+  end
+end
