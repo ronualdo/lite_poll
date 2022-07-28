73a79a6f6fdea86312ca0f39b2765726fad76922
ronualdo maciel <raxmac@gmail.com>
2022-07-28 20:26:13 -0300

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-IMAGE_REPO_URL="$(docker-compose run --rm -w /bitforma/infrastructure/terraform-aws-lite-poll web terraform output image_repo_url)"
-
-echo $IMAGE_REPO_URL
+IMAGE_REPO_URL="$(docker-compose run --rm -w /bitforma/infrastructure/terraform-aws-lite-poll web terraform output --raw image_repo_url)"
 
 docker build . -t $IMAGE_REPO_URL:$1
+
+aws ecr get-login-password --region us-west-2 | docker login --username AWS \
+  --password-stdin $IMAGE_REPO_URL:$1
+
+docker push $IMAGE_REPO_URL:$1
